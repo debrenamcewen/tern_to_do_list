db2d545fe2e20db57613a3acd7fe38596c38a0fb
Phillip Lorenzo <pllearns@gmail.com>
2016-08-22 16:08:15 -0700

    cleaner file structure with working express app

---
+++ routes/todo.js
@@ -0,0 +1,8 @@
+// var express = require('express');
+// var router = express.Router();
+// var database = require('../database');
+//
+// router.post('/todo', (req, res) => {
+//   const { title } = req.body
+//   database.addTo
+// })
