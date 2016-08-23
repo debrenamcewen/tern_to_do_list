056b1d48967f4592128bff207ae704550255ae97
Phillip Lorenzo <pllearns@gmail.com>
2016-08-22 17:55:02 -0700

    Created login page and began setup for simple auth

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var cookieSession = require('cookie-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var login = require('./routes/users');
 
 var app = express();
 
@@ -22,8 +25,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//authentication using passport
+// app.use(session({ secret: ''}));
+app.use(passport.initialize());
+app.use(passport.session());
+// app.use(flash());
+// require('./app.js')(app, passport);
+
 app.use('/', routes);
 app.use('/users', users);
+app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var cookieSession = require('cookie-session');
+var database = require('../database');
 
-/* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+router.get('/', function(req, res){
+    res.render('login');
 });
 
 module.exports = router;
